36f0c1c003ad9d344ced745f32211f034044c35b
Kian <kian@vsu.cc>
2017-02-11 17:33:14 +0800

    增加部分CURD方法

--- db/index.js
+++ db/index.js
@@ -15,10 +15,12 @@ class Db {
         this.modelName = lodash_1.upperFirst(name);
         try {
             let schema = require(path_1.join(modelPath, name)).schema;
-            console.log(schema);
             this.schema = new mongoose_1.Schema(schema);
             this.config = require(path_1.join(appPath, 'database'))['database'];
-            let db = mongoose_1.createConnection(this.config.host, this.config.name, this.config.port);
+            let db = mongoose_1.createConnection(this.config.host, this.config.name, this.config.port, {
+                user: this.config.user,
+                pass: this.config.password
+            });
             db.on('error', console.error.bind(console, '连接错误:'));
             db.once('open', function (e) {
                 console.log('connect mongod');
@@ -28,17 +30,65 @@ class Db {
         catch (e) {
         }
     }
-    add(title, callback) {
-        let Entity = new this.model();
-        Entity.title = title;
-        Entity.save(function (err) {
-            if (err) {
-                console.log(err);
-                callback(err);
-            }
-            else {
-                callback(null);
-            }
+    add(document) {
+        let Entity = new this.model(document);
+        return new Promise((resolve, reject) => {
+            Entity.save((err) => {
+                if (err) {
+                    console.log(err);
+                    resolve({
+                        error: -1,
+                        data: err,
+                        message: "add failed!"
+                    });
+                }
+                else {
+                    resolve({
+                        error: 0,
+                        data: Entity,
+                        message: "find successfully!"
+                    });
+                }
+            });
+        });
+    }
+    find(condition) {
+        return new Promise((resolve, reject) => {
+            this.model.find(condition, (err, documents) => {
+                if (err) {
+                    resolve({
+                        error: -1,
+                        data: err,
+                        message: "find failed!"
+                    });
+                }
+                else {
+                    resolve({
+                        error: 0,
+                        data: documents,
+                        message: "find successfully!"
+                    });
+                }
+            });
+        });
+    }
+    remove(condition) {
+        return new Promise((resolve, reject) => {
+            this.model.remove(condition, (err) => {
+                if (err) {
+                    resolve({
+                        error: -1,
+                        data: err,
+                        message: "remove failed!"
+                    });
+                }
+                else {
+                    resolve({
+                        error: 0,
+                        message: "remove successfully!"
+                    });
+                }
+            });
         });
     }
 }
--- db/index.ts
+++ db/index.ts
@@ -18,11 +18,14 @@ export class Db{
         this.modelName = upperFirst(name);
         try{
             let schema = require(join(modelPath, name)).schema;
-            console.log(schema);
+            // console.log(schema);
             this.schema = new Schema(schema);
             this.config = require(join(appPath, 'database'))['database'];
 
-            let db = createConnection(this.config.host, this.config.name, this.config.port);
+            let db = createConnection(this.config.host, this.config.name, this.config.port, {
+                user: this.config.user,
+                pass: this.config.password
+            });
 
             db.on('error', console.error.bind(console,'连接错误:'));
             db.once('open',function(e){
@@ -36,16 +39,66 @@ export class Db{
         }
     }
 
-    public add(title, callback){
-        let Entity=new this.model();
-        Entity.title=title;
-        Entity.save(function(err){
-            if(err){
-                console.log(err);
-                callback(err);
-            }else{
-                callback(null);
-            }
+    public add(document):Promise<any> {
+        let Entity=new this.model(document);
+        // Entity.aaa=123;
+        return new Promise((resolve, reject) => {
+            Entity.save((err) => {
+                if(err) {
+                    console.log(err);
+                    resolve({
+                        error: -1,
+                        data: err,
+                        message: "add failed!"
+                    });
+                } else {
+                    resolve({
+                        error: 0,
+                        data: Entity,
+                        message: "find successfully!"
+                    });
+                }
+            });
+        });
+    }
+
+    public find(condition): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.model.find(condition, (err, documents) => {
+                if(err) {
+                    resolve({
+                        error: -1,
+                        data: err,
+                        message: "find failed!"
+                    });
+                } else {
+                    resolve({
+                        error: 0,
+                        data: documents,
+                        message: "find successfully!"
+                    });
+                }
+            });
+        });
+
+    }
+
+    public remove(condition): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.model.remove(condition, (err) => {
+                if(err) {
+                    resolve({
+                        error: -1,
+                        data: err,
+                        message: "remove failed!"
+                    });
+                } else {
+                    resolve({
+                        error: 0,
+                        message: "remove successfully!"
+                    });
+                }
+            })
         });
     }
 }
